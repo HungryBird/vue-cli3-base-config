222134e9027dd02af2b5a098b67ca4b3e5791f2f
何俊洁 <381548325@qq.com>
2019-06-17 11:14:22 +0800

    '添加枚举文件'

---
+++ src/enumeration/index.js
@@ -0,0 +1,16 @@
+export const XM = {
+    STATE_A: [
+        {
+            label: '全部',
+            value: 'all'
+        },
+        {
+            label: '未完成',
+            value: '0'
+        },
+        {
+            label: '已完成',
+            value: '1'
+        }
+    ]
+}
